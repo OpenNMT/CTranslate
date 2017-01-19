7bdfe94d3c2b9a64e13fc90dac2bf0d7f7af5a51
Jean A. Senellart <jean.senellart@systrangroup.com>
2017-01-20 00:16:54 +0100

    lua unicode equivalents

---
+++ lib/unicode/unicode.hh
@@ -0,0 +1,26 @@
+#ifndef _UNICODE_HH
+#define _UNICODE_HH
+
+#include <cstdint>
+#include <map>
+#include <unordered_map>
+#include <vector>
+#include <string>
+
+typedef uint32_t unicode_code_point_t ;
+typedef std::map<unicode_code_point_t, std::vector<unicode_code_point_t> > map_of_list_t;
+typedef std::unordered_map<unicode_code_point_t, unicode_code_point_t> map_unicode;
+
+std::string cp_to_utf8(unicode_code_point_t u);
+unicode_code_point_t utf8_to_cp(const unsigned char* s, unsigned int &l);
+
+enum _type_letter { _letter_other, _letter_lower, _letter_upper };
+
+bool isSeparator(unicode_code_point_t u);
+bool isLetter(unicode_code_point_t u, _type_letter &tl);
+bool isNumber(unicode_code_point_t u);
+
+unicode_code_point_t getUpper(unicode_code_point_t u);
+unicode_code_point_t getLower(unicode_code_point_t u);
+
+#endif
---
+++ lib/unicode/unicode_data.hh
@@ -0,0 +1,13 @@
+#ifndef _UNICODE_DATA_HH
+#define _UNICODE_DATA_HH
+
+#include "unicode.hh"
+
+extern const map_of_list_t unidata_Number;
+extern const map_of_list_t unidata_LetterUpper;
+extern const map_of_list_t unidata_LetterLower;
+extern const map_of_list_t unidata_LetterOther;
+extern const map_of_list_t unidata_Separator;
+extern const map_unicode map_lower;
+
+#endif
