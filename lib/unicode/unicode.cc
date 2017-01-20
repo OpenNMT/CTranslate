f4378d78be75b7a045b14c380bedda0c3c61fc00
Guillaume Klein <guillaume.klein@systrangroup.com>
2017-01-20 10:27:17 +0100

    Harmonize coding style

--- lib/unicode/unicode.cc
+++ lib/unicode/unicode.cc
@@ -8,7 +8,8 @@ std::string cp_to_utf8(unicode_code_point_t uc)
 
   if (uc < 0x80)
   {
-    ret[0] = uc; ret[1] = 0; return ret;
+    ret[0] = uc; ret[1] = 0;
+    return ret;
   }
   else if (uc < 0x800)
   {
@@ -43,101 +44,140 @@ std::string cp_to_utf8(unicode_code_point_t uc)
     ret[4] = 0;
     return ret;
   }
+
   return "";
 }
 
 unicode_code_point_t utf8_to_cp(const unsigned char* s, unsigned int &l)
 {
-  if (*s == 0 || *s >= 0xfe) return 0;
-  if (*s <= 0x7f) { l=1; return *s; };
-  if (!s[1]) return 0;
-  if (*s < 0xe0) { l=2; return ((s[0] & 0x1f) << 6) + ((s[1] & 0x3f)); }
-  if (!s[2]) return 0;
-  if (*s < 0xf0) { l=3; return ((s[0] & 0x0f) << 12) + ((s[1] & 0x3f) << 6) + ((s[2] & 0x3f)); }
-  if (!s[3]) return 0;
-  if (*s < 0xf8) { l=4; return ((s[0] & 0x07) << 18) + ((s[1] & 0x3f) << 12) + ((s[2] & 0x3f) << 6)
-                               + ((s[3] & 0x3f)); }
+  if (*s == 0 || *s >= 0xfe)
+    return 0;
+  if (*s <= 0x7f)
+  {
+    l = 1;
+    return *s;
+  }
+  if (!s[1])
+    return 0;
+  if (*s < 0xe0)
+  {
+    l = 2;
+    return ((s[0] & 0x1f) << 6) + ((s[1] & 0x3f));
+  }
+  if (!s[2])
+    return 0;
+  if (*s < 0xf0)
+  {
+    l = 3;
+    return ((s[0] & 0x0f) << 12) + ((s[1] & 0x3f) << 6) + ((s[2] & 0x3f));
+  }
+  if (!s[3])
+    return 0;
+  if (*s < 0xf8)
+  {
+    l = 4;
+    return (((s[0] & 0x07) << 18) + ((s[1] & 0x3f) << 12) + ((s[2] & 0x3f) << 6)
+            + ((s[3] & 0x3f)));
+  }
+
   return 0; // Incorrect unicode
 }
 
 static bool _find_codepoint(unicode_code_point_t u, const map_of_list_t &map)
 {
-  map_of_list_t::const_iterator it=map.begin();
-  while (it!=map.end()) {
-    if (u>= it->first) {
-      unsigned int idx = ((u-it->first)>>4)+1;
-      if (idx<it->second.size()) {
-        unsigned int p = (u-it->first)&0xf;
-        return (((it->second[idx]<<p))&0x8000);
+  map_of_list_t::const_iterator it = map.begin();
+
+  while (it != map.end())
+  {
+    if (u >= it->first)
+    {
+      unsigned int idx = ((u - it->first) >> 4) + 1;
+      if (idx < it->second.size())
+      {
+        unsigned int p = (u - it->first) & 0xf;
+        return (((it->second[idx] << p)) & 0x8000);
       }
     }
+
     it++;
   }
+
   return 0;
 }
 
-bool isSeparator(unicode_code_point_t u)
+bool is_separator(unicode_code_point_t u)
 {
-  if (!u) return false;
+  if (!u)
+    return false;
   return (u >= 9 and u <= 13) or _find_codepoint(u, unidata_Separator);
 }
 
-bool isLetter(unicode_code_point_t u, _type_letter &tl)
+bool is_letter(unicode_code_point_t u, _type_letter &tl)
 {
-  if (!u) return false;
+  if (!u)
+    return false;
   // unicode letter or CJK Unified Ideograph
   if ((u>=0x4E00 && u<=0x9FD5) // CJK Unified Ideograph
-      || (u>=0x2F00 and u<=0x2FD5) // Kangxi Radicals
-      || (u>=0x2E80 and u<=0x2EFF) // CJK Radicals Supplement
-      || (u>=0x3040 and u<=0x319F) // Hiragana, Katakana, Bopomofo, Hangul Compatibility Jamo, Kanbun
-      || (u>=0x1100 and u<=0x11FF) // Hangul Jamo
-      || (u>=0xAC00 and u<=0xD7AF) // Hangul Syllables
-      || _find_codepoint(u, unidata_LetterOther)
-      ) {
+      || (u>=0x2F00 && u<=0x2FD5) // Kangxi Radicals
+      || (u>=0x2E80 && u<=0x2EFF) // CJK Radicals Supplement
+      || (u>=0x3040 && u<=0x319F) // Hiragana, Katakana, Bopomofo, Hangul Compatibility Jamo, Kanbun
+      || (u>=0x1100 && u<=0x11FF) // Hangul Jamo
+      || (u>=0xAC00 && u<=0xD7AF) // Hangul Syllables
+      || _find_codepoint(u, unidata_LetterOther))
+  {
     tl = _letter_other;
     return 1;
   }
-  if (_find_codepoint(u, unidata_LetterLower)) {
+  if (_find_codepoint(u, unidata_LetterLower))
+  {
     tl = _letter_lower;
     return 1;
   }
-  if (_find_codepoint(u, unidata_LetterUpper)) {
+  if (_find_codepoint(u, unidata_LetterUpper))
+  {
     tl = _letter_upper;
     return 1;
   }
+
   return 0;
 }
 
-bool isNumber(unicode_code_point_t u)
+bool is_number(unicode_code_point_t u)
 {
-  if (!u) return false;
+  if (!u)
+    return false;
   return (u >= 9 and u <= 13) or _find_codepoint(u, unidata_Number);
 }
 
 // convert unicode character to lowercase form if defined in unicodedata
-unicode_code_point_t getLower(unicode_code_point_t u)
+unicode_code_point_t get_lower(unicode_code_point_t u)
 {
   map_unicode::const_iterator it = map_lower.find(u);
-  if (it==map_lower.end()) return 0;
+  if (it == map_lower.end())
+    return 0;
   return it->second;
 }
 
 // convert unicode character to uppercase form if defined in unicodedata
 // dynamically reverse maplower if necessary
 static map_unicode map_upper;
-unicode_code_point_t getUpper(unicode_code_point_t u)
+
+unicode_code_point_t get_upper(unicode_code_point_t u)
 {
-  if (map_upper.size()==0) {
+  if (map_upper.size() == 0)
+  {
     map_unicode::const_iterator it;
-    for(it=map_lower.begin(); it!=map_lower.end(); it++) {
-      map_unicode::const_iterator jt=map_upper.find(it->second);
+    for (it = map_lower.begin(); it != map_lower.end(); it++)
+    {
+      map_unicode::const_iterator jt = map_upper.find(it->second);
       // reversing, we find smallest codepoint
-      if (jt==map_upper.end() || jt->second>it->first) {
-        map_upper[it->second]=it->first;
-      }
+      if (jt == map_upper.end() || jt->second>it->first)
+        map_upper[it->second] = it->first;
     }
   }
+
   map_unicode::const_iterator it = map_upper.find(u);
-  if (it==map_upper.end()) return 0;
+  if (it == map_upper.end())
+    return 0;
   return it->second;
 }
