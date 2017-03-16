66e0da1c2b7492aae041b0b95bf8e870e5f741ab
Guillaume Klein <guillaume.klein@systrangroup.com>
2017-03-16 10:14:43 -0700

    Add missing changes

--- src/cuda/Kernels.cu
+++ src/cuda/Kernels.cu
@@ -5,28 +5,47 @@ namespace onmt
   namespace cuda
   {
 
-    // This kernel replicates the vector `vector` `dim` times in the matrix `matrix`.
-    // The storage order is assumed to be col-major.
+    // This kernel replicates the vector `vector` in the matrix `matrix`.
+    // Credits to Robert Crovella (http://stackoverflow.com/a/25453485).
     template <typename T>
-    __global__ void replicate_kernel(const T* vector, int len, T* matrix, int dim)
+    __global__ void replicate_kernel(const T* vector, int len, T* matrix, int rows, int cols)
     {
-      int idx = threadIdx.x + blockIdx.x * dim;
-      T myval = vector[blockIdx.x];
-      while (idx < ((blockIdx.x + 1) * dim))
+      int dim;
+
+      if (len == cols)
       {
-        matrix[idx] = myval;
-        idx += blockDim.x;
+        dim = rows;
+        int idx = threadIdx.x + blockIdx.x * dim;
+        T myval = vector[blockIdx.x];
+        while (idx < ((blockIdx.x + 1) * dim))
+        {
+          matrix[idx] = myval;
+          idx += blockDim.x;
+        }
+      }
+      else
+      {
+        dim = cols;
+        int idx = threadIdx.x + blockDim.x * blockIdx.x;
+        T myval = vector[idx % len];
+        while (idx < dim * len){
+          matrix[idx] = myval;
+          idx += gridDim.x * blockDim.x;
+        }
       }
     }
 
     template <typename T>
-    void replicate(const T* vector, int len, T* matrix, int dim)
+    void replicate(const T* vector, int len, T* matrix, int rows, int cols)
     {
-      // TODO: optimize kernel parameters?
-      replicate_kernel<<<len, 256>>>(vector, len, matrix, dim);
+      int blocks = 500;
+      if (len == cols)
+        blocks = len;
+
+      replicate_kernel<<<blocks, 512>>>(vector, len, matrix, rows, cols);
     }
 
-    template void replicate<float>(const float* vector, int len, float* matrix, int dim);
+    template void replicate<float>(const float* vector, int len, float* matrix, int rows, int cols);
 
   }
 }
